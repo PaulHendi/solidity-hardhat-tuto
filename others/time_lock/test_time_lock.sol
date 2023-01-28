e157376a335fc083b84260cb25ca1518e9dede86
PaulHendi <paulhendi25@gmail.com>
2023-01-28 07:28:07 +0100

    Added time tock tuto, tests incoming ..

---
+++ others/time_lock/test_time_lock.sol
@@ -0,0 +1,25 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract TestTimeLock {
+    address public timeLock;
+    bool public canExecute;
+    bool public executed;
+
+    constructor(address _timeLock) {
+        timeLock = _timeLock;
+    }
+
+    fallback() external {}
+
+    function func() external payable {
+        require(msg.sender == timeLock, "not time lock");
+        require(canExecute, "cannot execute this function");
+        executed = true;
+    }
+
+    function setCanExecute(bool _canExecute) external {
+        canExecute = _canExecute;
+    }
+}
